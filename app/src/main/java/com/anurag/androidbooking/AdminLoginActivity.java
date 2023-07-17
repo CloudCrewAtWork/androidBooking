61fa183a3ebeae4e9cd59dd94737b8a2dd98cb8e
CloudCrewAtWork <anurag.sai540@gmail.com>
2023-07-16 20:05:30 -0600

    step1 working and all packages updated

--- app/src/main/java/com/anurag/androidbooking/AdminLoginActivity.java
+++ app/src/main/java/com/anurag/androidbooking/AdminLoginActivity.java
@@ -1,15 +1,16 @@
 package com.anurag.androidbooking;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.R;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.Objects;
 
 public class AdminLoginActivity extends AppCompatActivity {
 
@@ -17,12 +18,13 @@ public class AdminLoginActivity extends AppCompatActivity {
     EditText adminEditText;
     String id;
 
+    @SuppressLint("MissingSuperCall")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_login);
-        db = FirebaseFirestore.getInstance();
-        adminEditText = findViewById(R.id.adminID);
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_admin_login);
+//        db = FirebaseFirestore.getInstance();
+//        adminEditText = findViewById(R.id.adminID);
     }
 
     public void loginAdmin(View view) {
@@ -31,20 +33,17 @@ public class AdminLoginActivity extends AppCompatActivity {
         db.collection("AdminId")
                 .whereEqualTo("Aid", id)
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            if (task.getResult().size() == 1) {
-                                //start new Intent
-                                Intent adminIntent = new Intent(AdminLoginActivity.this, AdminFinalViewActivity.class);
-                                startActivity(adminIntent);
-                            } else {
-
-                            }
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        if (Objects.requireNonNull(task.getResult()).size() == 1) {
+                            //start new Intent
+                            Intent adminIntent = new Intent(AdminLoginActivity.this, AdminFinalViewActivity.class);
+                            startActivity(adminIntent);
                         } else {
-                            Log.d("Query", "Error getting documents: ", task.getException());
+
                         }
+                    } else {
+                        Log.d("Query", "Error getting documents: ", task.getException());
                     }
                 });
     }
